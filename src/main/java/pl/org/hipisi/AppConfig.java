3cad19f7e73f6ce2d91a995d1c56b3ab5221ecbd
Beret Adam Manczuk <beret@hipisi.org.pl>
2017-09-15 11:21:59 +0200

    add static file handler

--- src/main/java/pl/org/hipisi/AppConfig.java
+++ src/main/java/pl/org/hipisi/AppConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -23,6 +25,18 @@ public class AppConfig extends WebMvcConfigurerAdapter {
 		InternalResourceViewResolver viewResolver =	new InternalResourceViewResolver();
 		viewResolver.setPrefix("/WEB-INF/views/");
 		viewResolver.setSuffix(".jsp");
-		return viewResolver; }
+		return viewResolver; 
+	}
+	
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**")
+		.addResourceLocations("/WEB-INF/resources/").setCachePeriod(31556926);    
+	}
 
 }
